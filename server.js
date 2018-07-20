4b906f6f6c0db60ae3c34b67addcfae89054e3b9
tonyreaves <tonyfreaves@gmail.com>
2018-07-19 21:18:32 -0700

    who knows

--- server.js
+++ server.js
@@ -9,8 +9,6 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.listen(PORT, function(){});
-
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
