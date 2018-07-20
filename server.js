39ddf7b0c93c42bb6456b1d843f76f13a83b2523
tonyreaves <tonyfreaves@gmail.com>
2018-07-19 21:14:19 -0700

    what is even happening

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function(){});
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
